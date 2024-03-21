e70da50bee56fa561fde9d158e2f3d903596e63f
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-21 17:21:23 +0700

    (3) Validating request and selectively responding

--- README.md
+++ README.md
@@ -9,11 +9,15 @@
 - `let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();` This line reads the HTTP request from the client. The `lines()` method reads the request line by line. The `map()` method transforms each line into a `Result<String, io::Error>`. The `unwrap()` method is used to unwrap the Result and get the String value. The `take_while()` method takes lines from the iterator until a line that is empty is encountered. The `collect()` method collects the lines into a `Vec<String>`.
 - `println!("Request: {:#?}", http_request);` This line prints the HTTP request to the console. The {:#?} syntax is used to pretty-print the request.
 
-### 2. new `handle_connection` method
+### 2. new `handle_connection` method to parse a 200 OK response
 
 ![img1](assets/commit2.png)
 
 - The function then constructs an HTTP response. The status line of the response is set to "HTTP/1.1 200 OK", indicating a successful HTTP request. The contents of the response are read from a file named "hello.html". The length of the contents is calculated and set as the "Content-Length" header of the response.
 - The `handle_connection` function is called for each incoming connection in the `main` method. The `main` method listens for incoming TCP connections on the IP address "127.0.0.1" and port "7878". For each incoming connection, the `handle_connection` function is called to handle the connection.
 
-### 3.
+### 3. new `handle_connection` method to parse a 404 Not Found response
+
+![img2](assets/commit3.png)
+
+- The function then constructs an HTTP response. The status line of the response is set to "HTTP/1.1 404 NOT FOUND", indicating an unsuccessful HTTP request. The contents of the response are read from a file named "404.html". The length of the contents is calculated and set as the "Content-Length" header of the response.
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();for stream in listener.incoming() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
     }
 }
+
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream); let http_request: Vec<_> = buf_reader
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap()) 
         .take_while(|line| !line.is_empty()) 
         .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap(); let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let (status_line, contents) = if http_request.get(0).map_or(false, |line| line.starts_with("GET")) {
+        let resource = http_request.get(0).unwrap().split_whitespace().nth(1).unwrap();
+        let filename = if resource == "/" {
+            "hello.html"
+        } else {
+            &resource[1..] // remove leading '/'
+        };
+        match fs::read_to_string(filename) {
+            Ok(contents) => ("HTTP/1.1 200 OK", contents),
+            Err(_) => ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("notfound.html").unwrap()),
+        }
+    } else {
+        ("HTTP/1.1 400 BAD REQUEST", String::from("Bad request"))
+    };
 
-    stream.write_all(response.as_bytes()).unwrap(); 
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}", status_line=status_line, length=length, contents=contents);
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
